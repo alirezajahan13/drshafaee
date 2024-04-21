2c151c3e0106bb80ce09126803a76cd2e48d32bf
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2024-04-21 17:11:50 +0330

    one

--- footer.php
+++ footer.php
@@ -17,12 +17,12 @@
 			<?php require 'svg/logosvg.php'; ?>
 		</div>
 		<div class="sectionLeft">
-			<a href="https://www.drshafaee.com/contact/" class="generalButton CounselingBtn" target="_blank">جهت ارتباط با ما</a>
+			<a href="https://www.drshafaee.com/contact/" class="generalButton CounselingBtn" target="_blank">للتواصل معنا</a>
 		</div>
 	</div>
 		<div class="mainFooter mainView">
 			<div class="footerCulomn1">
-				<h3 class="mediumWeight">راه های ارتباطی</h3>
+				<h3 class="mediumWeight">طرق التواصل</h3>
 				<div class="innerGeneralFooter">
 						<a href="tel:+9886124856" class="lightParagraph generalCU">
 							<!-- <svg width="17px" height="17px" fill="#1d3141" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 287.32 287.32" style="enable-background:new 0 0 287.32 287.32" xml:space="preserve"><path d="M267.749 191.076c-14.595-11.729-27.983-17.431-40.93-17.431-18.729 0-32.214 11.914-44.423 24.119-1.404 1.405-3.104 2.06-5.349 2.06-10.288.001-28.387-12.883-53.794-38.293-29.89-29.892-41.191-48.904-33.592-56.506 20.6-20.593 27.031-41.237-4.509-80.462C73.861 10.51 62.814 3.68 51.38 3.68c-15.42 0-27.142 12.326-37.484 23.202-1.788 1.88-3.477 3.656-5.133 5.312-11.689 11.688-11.683 37.182.017 68.2 12.837 34.033 38.183 71.055 71.37 104.247 25.665 25.663 53.59 46.403 80.758 60.328 23.719 12.158 46.726 18.672 64.783 18.672h.007c11.3 0 20.479-2.465 26.541-7.478 12.314-10.181 35.234-29.039 35.081-51.439-.084-12.014-6.667-23.273-19.571-33.648z"></path></svg> -->
@@ -42,7 +42,7 @@
 						</a>
 						<a href="https://goo.gl/maps/CSEirNfMy3aM3Esd9" class="lightParagraph generalCU">
 							<!-- <svg class="addressSvgRes" width="34px" height="34px" fill="#1d3141" xmlns="http://www.w3.org/2000/svg" data-name="Layer 3" viewBox="0 0 24 24"><path d="M12 1.015a8.973 8.973 0 0 0-8.963 8.963c0 3.647 2.647 6.675 5.452 9.88.887 1.015 1.806 2.065 2.624 3.127a1.12 1.12 0 0 0 1.775 0c.818-1.062 1.737-2.112 2.624-3.127C18.315 16.653 20.963 13.625 20.963 9.978A8.973 8.973 0 0 0 12 1.015Zm0 12.324a3.361 3.361 0 1 1 3.361-3.361A3.361 3.361 0 0 1 12 13.339Z"></path></svg> -->
-							<span>تهران، خیابان مطهری، نرسیده به فجر، پلاک ۲۸۲، ساختمان پزشکان کوه نور، طبقه۴، واحد۱۴</span>
+							<span>طهران، شارع مطهري، قبل شارع الفجر، رقم ۲۸۲، مبنى أطباء كوه نور، الطابق الرابع، شقة ۱۴</span>
 						</a>
 				</div>
 			</div>
@@ -50,47 +50,47 @@
 				<h3 class="mediumWeight">خدمات</h3>
 				<div class="innerLinkFooter">
 					<li class="noListStyle">
-						<a href="https://www.drshafaee.com/service/%d8%ac%d8%b1%d8%a7%d8%ad%db%8c-%d8%a8%db%8c%d9%86%db%8c/" target="_blank">جراحی بینی</a>
+						<a href="https://www.drshafaee.com/service/%d8%ac%d8%b1%d8%a7%d8%ad%db%8c-%d8%a8%db%8c%d9%86%db%8c/" target="_blank">جراحة الأنف</a>
 					</li>
 					<li class="noListStyle">
-						<a href="#">تزریق بوتاکس</a>
+						<a href="#">حقن البوتاكس</a>
 					</li>
 					<li class="noListStyle">
-						<a href="#">ماموپلاستی</a>
+						<a href="#">ماموبلاستی</a>
 					</li>
 					<li class="noListStyle">
-						<a href="#">لیفت صورت</a>
+						<a href="#">لیفت الوجه</a>
 					</li>
 					<li class="noListStyle">
-						<a href="#">ابدومینوپلاستی</a>
+						<a href="#">ابدومینوبلاستی</a>
 					</li>
 				</div>
 			</div>
 			<div class="footerCulomn3">
-				<h3  class="mediumWeight">لینک ها</h3>
+				<h3  class="mediumWeight">الروابط</h3>
 				<div class="innerLinkFooter">
 						<li class="noListStyle">
-							<a href="https://www.drshafaee.com/">خانه</a>
+							<a href="https://www.drshafaee.com/">الرئيسية</a>
 						</li>
-						<li class="noListStyle">
+						<!-- <li class="noListStyle">
 							<a href="https://www.drshafaee.com/blog/">مجله</a>
+						</li> -->
+						<li class="noListStyle">
+							<a href="https://www.drshafaee.com/portfolio/">أعمال سابقة</a>
 						</li>
 						<li class="noListStyle">
-							<a href="https://www.drshafaee.com/portfolio/">نمونه کارها</a>
+							<a href="https://www.drshafaee.com/contact/">اتصل بنا</a>
 						</li>
 						<li class="noListStyle">
-							<a href="https://www.drshafaee.com/contact/">تماس با ما</a>
+							<a href="https://www.drshafaee.com/about/">عن الدكتور</a>
 						</li>
 						<li class="noListStyle">
-							<a href="https://www.drshafaee.com/about/">درباره دکتر</a>
-						</li>
-						<li class="noListStyle">
-							<a href="https://drshafaee.com/plastic-surgery/">بهترین دکتر جراح پلاستیک در تهران</a>
+							<a href="https://drshafaee.com/plastic-surgery/">أفضل طبيب جراح تجميل في طهران</a>
 						</li>
 				</div>
 			</div>
 			<div class="footerCulomn4">
-				<h3  class="mediumWeight">شبکه های اجتماعی</h3>
+				<h3  class="mediumWeight">الشبكات الاجتماعية</h3>
 				<div class="innerIconFooter">
 					<div class="contactIconSocial">
 						<a href="https://www.instagram.com/dr.yousefshafaee/" target="_blank">
@@ -111,8 +111,8 @@
 					</div>
 				</div>
 				<div class="newsLettersBoxFooter">
-					<h3 class="mediumWeight">خبرنامه</h3>
-					<?php echo do_shortcode ('[contact-form-7 id="22" title="خبرنامه"]') ?>
+					<h3 class="mediumWeight">النشرة الإخبارية</h3>
+					<?php echo do_shortcode ('[contact-form-7 id="796832f" title="خبرنامه"]') ?>
 				</div>
 			</div>
 		</div>
@@ -121,12 +121,12 @@
 	<div class="footerCopyRightParent">
 		<div class="footerCopyRight mainView">
 			<div class="footerCopyRightPa">
-				<p>تمامی حقوق وبسایت برای کلینیک دکتر شفائی محفوظ است. <a href="https://idechy.ir/" target="_blank">طراحی وبسایت توسط ایده‌چی</a></p>
+				<p>جميع حقوق الموقع محفوظة لعيادة الدكتور شفائي. <a href="https://idechy.ir/" target="_blank">تصميم الموقع الإلكتروني من قبل ایده‌چی</a></p>
 			</div>
 			<div class="footerCopyRightLink">
-				<a href="https://www.drshafaee.com/contact/">تماس با ما</a>
-				<a href="https://www.drshafaee.com/about/">درباره دکتر</a>
-				<a href="https://www.drshafaee.com/portfolio/">نمونه کارها</a>
+				<a href="https://www.drshafaee.com/contact/">اتصل بنا</a>
+				<a href="https://www.drshafaee.com/about/">عن الدكتور</a>
+				<a href="https://www.drshafaee.com/portfolio/">أعمال سابقة</a>
 			</div>
 		</div>
 	</div>
